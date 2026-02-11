c8d816701c69aedb7654baf6d7d908df6eb9d9f8
ikrsx <krishal.sh@tuta.io>
2026-02-11 22:56:29 +0530

    updated niri script

--- niri/scripts/idle_manager.sh
+++ niri/scripts/idle_manager.sh
@@ -12,18 +12,17 @@ status=$(cat "$BAT_STATUS" 2>/dev/null)
 # If the battery is discharging, use shorter timeouts to preserve battery life.
 if [ "$status" = "Discharging" ]; then
   # After 2 minutes of idle, dim the screen to 5% and save the previous brightness.
+  # After 3 minutes, turn off the monitors via niri.
   # After 4 minutes, lock the screen.
-  # After 7 minutes, turn off the monitors via niri.
   # After 10 minutes, suspend the system.
-  # When activity resumes, restore the saved brightness level.
   # When activity resumes, restore the saved brightness level && turn the monitors back on via niri.
-  swayidle timeout 120 'brightnessctl -s set 5%' timeout 240 'hyprlock' timeout 420 'niri msg action power-off-monitors' timeout 600 'systemctl suspend' resume 'brightnessctl -r && niri msg action power-on-monitors'
+  swayidle timeout 120 'brightnessctl --save set 5%' resume 'brightnessctl -r' timeout 180 'niri msg action power-off-monitors' resume 'niri msg action power-on-monitors'  timeout 240 'hyprlock' timeout 600 'systemctl suspend'
 else
   # If the system is charging, full, or status is unknown, use longer timeouts.
   # After 5 minutes of idle, dim the screen to 5% and save the previous brightness.
-  # After 8 minutes, lock the screen.
-  # After 15 minutes, turn off the monitors via niri.
-  # After 30 minutes, suspend the system.
+  # After 6 minutes, turn off the monitors via niri.
+  # After 7 minutes lock the screen.
+  # After 15 minutes, suspend the system.
   # When activity resumes, restore the saved brightness level && turn the monitors back on via niri.
-  swayidle timeout 300 'brightnessctl -s set 5%' timeout 480 'hyprlock' timeout 900 'niri msg action power-off-monitors' timeout 1800 'systemctl suspend' resume 'brightnessctl -r && niri msg action power-on-monitors'
+  swayidle timeout 300 'brightnessctl --save set 5%' resume 'brightnessctl -r' timeout 360 'niri msg action power-off-monitors' resume 'niri msg action power-on-monitors'  timeout 420 'hyprlock' timeout 900 'systemctl suspend'
 fi
